a945589b6633cdffe16db586875c0ea920beb6b1
msvenss <matilda.svensson@ffcg.se>
2016-10-05 20:11:20 +0200

    added another rule

---
+++ HPBookKata/DiscountRules/DiscountRuleNoDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPBookKata.DiscountRules
+{
+   public class DiscountRuleNoDiscount : IDiscountRule
+    {
+       public decimal DiscountPercent { get; }
+       public bool CanBeUsed(Purchase purchase)
+       {
+            return purchase.ItemRows.Count() <= 1;
+        }
+
+       public int Order => 100;
+    }
+}
