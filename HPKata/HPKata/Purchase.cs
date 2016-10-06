4ef82d32d7e08dccbc2a44046fa7fddfd498c062
msvenss <matilda.svensson@ffcg.se>
2016-10-06 11:29:50 +0200

    refactor

--- HPKata/HPKata/Purchase.cs
+++ HPKata/HPKata/Purchase.cs
@@ -16,28 +16,23 @@ namespace HPKata
         {
             var newGroupedItemList = new List<PurchaseItem>();
 
-          List<IGrouping<string, PurchaseItem>> checkingList = itemRows.GroupBy(x => x.ItemToBuy.Title).ToList();
+          var checkingList = itemRows.GroupBy(x => x.ItemToBuy.Title).ToList();
 
             if (checkingList.Any(x => x.Count() > 1))
             {
                 foreach (var row in checkingList)
                 {
-                    int totNrOfItems = 0;
                     var book = row.First().ItemToBuy;
-                    foreach (var nrOfItems in row)
-                    {    
-                        totNrOfItems += nrOfItems.NrOfItems;
-                    }
-                    if (totNrOfItems > 0) { 
-                    PurchaseItem itemToAdd = new PurchaseItem(book,totNrOfItems);
+                    var totNrOfItems = row.Sum(nrOfItems => nrOfItems.NrOfItems);
+                    if (totNrOfItems <= 0) continue;
+                    var itemToAdd = new PurchaseItem(book,totNrOfItems);
                     newGroupedItemList.Add(itemToAdd);
-                        }
                 }
                 return newGroupedItemList;
             }
             else
             {
-                List<PurchaseItem> result = itemRows.ToList();
+                var result = itemRows.ToList();
                 if (itemRows.Any(x => x.NrOfItems == 0))
                 {
                    result.RemoveAll(x => x.NrOfItems == 0);
